c4352bf699fffcaf38d0e1d7c0f61c3fc02fc102
cuichuang <cuichuang01@gmail.com>
2022-12-06 11:08:37 +0800

    第一次提交

---
+++ mall-admin/src/main/java/com/macro/mall/controller/PmsProductController.java
@@ -0,0 +1,147 @@
+package com.macro.mall.controller;
+
+import com.macro.mall.common.api.CommonPage;
+import com.macro.mall.common.api.CommonResult;
+import com.macro.mall.dto.PmsProductParam;
+import com.macro.mall.dto.PmsProductQueryParam;
+import com.macro.mall.dto.PmsProductResult;
+import com.macro.mall.model.PmsProduct;
+import com.macro.mall.service.PmsProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 商品管理Controller
+ * Created by macro on 2018/4/26.
+ */
+@Controller
+@Api(tags = "PmsProductController", description = "商品管理")
+@RequestMapping("/product")
+public class PmsProductController {
+    @Autowired
+    private PmsProductService productService;
+
+
+    @ApiOperation("查询商品")
+    @GetMapping("/list")
+    @ResponseBody
+    public CommonResult<CommonPage<PmsProduct>> getList(PmsProductQueryParam productQueryParam,
+                                                        @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                        @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<PmsProduct> productList = productService.list(productQueryParam, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(productList));
+    }
+
+    @ApiOperation("根据商品id获取商品编辑信息")
+    @GetMapping("/updateInfo/{id}")
+    @ResponseBody
+    public CommonResult<PmsProductResult> getUpdateInfo(@PathVariable Long id) {
+        PmsProductResult productResult = productService.getUpdateInfo(id);
+        return CommonResult.success(productResult);
+    }
+
+    @ApiOperation("根据商品名称或货号模糊查询")
+    @GetMapping("/simpleList")
+    @ResponseBody
+    public CommonResult<List<PmsProduct>> getList(String keyword) {
+        List<PmsProduct> productList = productService.list(keyword);
+        return CommonResult.success(productList);
+    }
+
+    @ApiOperation("创建商品")
+    @PostMapping("/create")
+    @ResponseBody
+    public CommonResult create(@RequestBody PmsProductParam productParam, BindingResult bindingResult) {
+        int count = productService.create(productParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("更新商品")
+    @PutMapping("/update/{id}")
+    @ResponseBody
+    public CommonResult update(@PathVariable Long id, @RequestBody PmsProductParam productParam, BindingResult bindingResult) {
+        int count = productService.update(id, productParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+
+    @ApiOperation("批量修改审核状态")
+    @PutMapping("/update/verifyStatus")
+    @ResponseBody
+    public CommonResult updateVerifyStatus(@RequestParam("ids") List<Long> ids,
+                                           @RequestParam("verifyStatus") Integer verifyStatus,
+                                           @RequestParam("detail") String detail) {
+        int count = productService.updateVerifyStatus(ids, verifyStatus, detail);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量上下架")
+    @PutMapping("/update/publishStatus")
+    @ResponseBody
+    public CommonResult updatePublishStatus(@RequestParam("ids") List<Long> ids,
+                                            @RequestParam("publishStatus") Integer publishStatus) {
+        int count = productService.updatePublishStatus(ids, publishStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量推荐商品")
+    @PutMapping("/update/recommendStatus")
+    @ResponseBody
+    public CommonResult updateRecommendStatus(@RequestParam("ids") List<Long> ids,
+                                              @RequestParam("recommendStatus") Integer recommendStatus) {
+        int count = productService.updateRecommendStatus(ids, recommendStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量设为新品")
+    @PutMapping("/update/newStatus")
+    @ResponseBody
+    public CommonResult updateNewStatus(@RequestParam("ids") List<Long> ids,
+                                        @RequestParam("newStatus") Integer newStatus) {
+        int count = productService.updateNewStatus(ids, newStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量修改删除状态")
+    @PutMapping("/update/deleteStatus")
+    @ResponseBody
+    public CommonResult updateDeleteStatus(@RequestParam("ids") List<Long> ids,
+                                           @RequestParam("deleteStatus") Integer deleteStatus) {
+        int count = productService.updateDeleteStatus(ids, deleteStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+}
